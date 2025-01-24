e5fbcd632794210b0b26a1494faf412ae8a98a09
이지은 <tori@ryanlab.kr>
2025-01-24 16:43:50 +0900

    [feat] Schedule dto 추가

---
+++ src/main/java/com/example/scheduler/dto/ScheduleRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.scheduler.dto;
+
+import java.time.LocalDateTime;
+
+public class ScheduleRequestDto {
+
+    private String writer;
+    private String todo;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/example/scheduler/dto/ScheduleResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.scheduler.dto;
+
+import java.time.LocalDateTime;
+
+public class ScheduleResponseDto {
+
+    private String writer;
+    private String todo;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
