89fd614cceef76808544c89fe3cf6a09468d3204
이지은 <tori@ryanlab.kr>
2025-01-31 17:39:15 +0900

    [feat] null 체크 및 특정 패턴 검증 수행

--- src/main/java/com/example/scheduler/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/scheduler/dto/ScheduleRequestDto.java
@@ -1,13 +1,24 @@
 package com.example.scheduler.dto;
 
+import jakarta.validation.Valid;
 import lombok.Getter;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Getter
 public class ScheduleRequestDto {
 
+    @Valid
     private UserRequestDto user;
+
+    @NotNull(message = "Todo cannot be null")
+    @Size(min = 2, max = 200, message = "Todo should be between 2 and 200 characters")
     private String todo;
+
+    @NotNull(message = "Password cannot be null")
     private String password;
+
     private Long userId;
     private int pageNumber;
     private int pageSize;
